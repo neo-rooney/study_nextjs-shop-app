9a227b12ddbbf563b5fe2633598083e6cbbe79bf
rooney <bch3454@gmail.com>
2023-12-04 14:57:38 +0900

    Feat: Input 컴포넌트 생성

--- src/app/(auth)/login/LoginClient.jsx
+++ src/app/(auth)/login/LoginClient.jsx
@@ -1,23 +1,59 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import LogoPath from "@/assets/colorful.svg";
 import styles from "./Auth.module.scss";
 import Loader from "@/components/loader/Loader";
+import Input from "@/components/input/Input";
+import { useRouter } from "next/navigation";
 
 const LoginClient = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isAutoLogin, setIsAutoLogin] = useState(false);
+
+  const router = useRouter();
+
+  const redirectUser = () => {
+    router.push("/");
+  };
+
+  const loginUser = () => {};
+  const signInWithGoogle = () => {};
+
   return (
     <>
-      <Loader />
+      {/* <Loader /> */}
       <section className={styles.page}>
         <div className={styles.container}>
           <h1 className={styles.logo}>
             <Image src={LogoPath} alt="logo" priority />
           </h1>
           <form className={styles.form}>
-            {/* Input */}
-            Input
+            <Input
+              email
+              icon="letter"
+              id="email"
+              name="email"
+              label="이메일"
+              placeholder="아이디(이메일)"
+              className={styles.control}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              password
+              icon="lock"
+              id="password"
+              name="password"
+              label="비밀번호"
+              placeholder="비밀번호"
+              className={styles.control}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <div className={styles.group}>
               {/* 자동 로그인, 비밀번호 수정 */}
               자동 로그인, 비밀번호 수정
