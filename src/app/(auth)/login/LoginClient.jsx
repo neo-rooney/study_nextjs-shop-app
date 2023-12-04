e9339f46bfc038282fb711ae34a04316c9e527d7
rooney <bch3454@gmail.com>
2023-12-04 14:05:27 +0900

    Feat: 로그인 UI

---
+++ src/app/(auth)/login/LoginClient.jsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import LogoPath from "@/assets/colorful.svg";
+import styles from "./Auth.module.scss";
+
+const LoginClient = () => {
+  return (
+    <section className={styles.page}>
+      <div className={styles.container}>
+        <h1 className={styles.logo}>
+          <Image src={LogoPath} alt="logo" priority />
+        </h1>
+        <form className={styles.form}>
+          {/* Input */}
+          Input
+          <div className={styles.group}>
+            {/* 자동 로그인, 비밀번호 수정 */}
+            자동 로그인, 비밀번호 수정
+          </div>
+          <div className={styles.buttonGroup}>
+            {/* Button */}
+            Button
+            <div>{/* Button */}</div>
+            Button
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default LoginClient;
